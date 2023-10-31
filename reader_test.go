38d4ba1110608627cebacd921d0b88a78e301132
Jens Zeilund <jzs@sketchground.dk>
2023-10-31 13:03:12 +0100

    add pmtiles extract for tests

--- reader_test.go
+++ reader_test.go
@@ -7,18 +7,22 @@ import (
 )
 
 func TestReader(t *testing.T) {
-	tiles, err := libpmtiles.Open("/home/jzs/20231025.pmtiles")
+	tiles, err := libpmtiles.Open("extracts/samsoe_test.pmtiles")
 	if err != nil {
 		t.Fatalf("expect no err, got %v", err)
 	}
 
-	tiledata, err := tiles.GetTile(1, 1, 1, "mvt")
+	tiledata, err := tiles.GetTile(0, 0, 0, "mvt")
 	if err != nil {
 		t.Fatalf("Expect no err, got: %v", err)
 	}
 
 	if tiledata == nil {
 		t.Fatalf("Expect tile data, got nothing")
+	}
+
+	if len(tiledata) == 0 {
+		t.Fatalf("Expect to find tile data, got 0 bytes back")
 	}
 
 	log.Printf("%+v", tiles)
